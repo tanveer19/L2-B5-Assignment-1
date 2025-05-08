762cf98d6c51d6ad9488435f0f6125556df10fa1
tanveer19 <thjbd19@gmail.com>
2025-05-08 15:25:09 +0600

    initial commit

---
+++ Assignment-01.ts
@@ -0,0 +1,20 @@
+function formatString(input: string, toUpper?: boolean): string {
+  if (toUpper || toUpper == null) {
+    return input.toUpperCase();
+  } else {
+    return input.toLowerCase();
+  }
+}
+
+function filterByRating(
+  items: { title: string; rating: number }[]
+): { title: string; rating: number }[] {
+  return items.filter((item) => item.rating >= 4);
+}
+const books = [
+  { title: "Book A", rating: 4.5 },
+  { title: "Book B", rating: 3.2 },
+  { title: "Book C", rating: 5.0 },
+];
+
+console.log(filterByRating(books));
