c99cf360ddc0c265ca7845e1cb188f36546fb201
tanveer19 <thjbd19@gmail.com>
2025-05-08 18:35:36 +0600

    3

--- Assignment-01.ts
+++ Assignment-01.ts
@@ -11,10 +11,17 @@ function filterByRating(
 ): { title: string; rating: number }[] {
   return items.filter((item) => item.rating >= 4);
 }
-const books = [
-  { title: "Book A", rating: 4.5 },
-  { title: "Book B", rating: 3.2 },
-  { title: "Book C", rating: 5.0 },
-];
+// const books = [
+//   { title: "Book A", rating: 4.5 },
+//   { title: "Book B", rating: 3.2 },
+//   { title: "Book C", rating: 5.0 },
+// ];
 
-console.log(filterByRating(books));
+// console.log(filterByRating(books));
+
+function concatenateArrays<T>(...arrays: T[][]): T[] {
+  return arrays.flat();
+}
+
+console.log(concatenateArrays(["a", "b"], ["c"]));
+console.log(concatenateArrays([1, 2], [3, 4], [5]));
